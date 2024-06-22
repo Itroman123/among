f0fa0671bd38dc02361793c2c5f2d66a4f6cca9b
Itroman123 <143260109+Itroman123@users.noreply.github.com>
2024-06-22 21:12:07 +0300

    ....

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 function HideText() {
    const Text1 = document.getElementById("Tex1");
-
    let audio = new Audio();
    audio.src = './round-start.mp3';
    audio.autoplay = true;
-   
-
-
-   if (Text1.style.visibility === "hidden") {
+    if (Text1.style.visibility === "hidden") {
        Text1.style.visibility = "visible";
    } else {
        Text1.style.visibility = "hidden";
